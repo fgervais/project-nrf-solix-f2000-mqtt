aceaf0a188b705bca17bffc92bbe951c874cb53c
Francois Gervais <francoisgervais@gmail.com>
2023-07-05 12:06:04 -0400

    template: remove the button handling

--- app/src/main.c
+++ app/src/main.c
@@ -2,12 +2,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/pm/device.h>
 
-#include <app_event_manager.h>
-
-#define MODULE main
-#include <caf/events/module_state_event.h>
-#include <caf/events/button_event.h>
-
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
@@ -22,13 +16,6 @@ void main(void)
 {
 	int ret;
 	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-
-
-	if (app_event_manager_init()) {
-		LOG_ERR("Event manager not initialized");
-	} else {
-		module_set_state(MODULE_STATE_READY);
-	}
 
 	if (!device_is_ready(led.port)) {
 		return;
@@ -48,23 +35,3 @@ void main(void)
 
 	LOG_INF("PM_DEVICE_ACTION_SUSPEND");
 }
-
-static bool event_handler(const struct app_event_header *eh)
-{
-	int ret;
-	const struct button_event *evt;
-
-	if (is_button_event(eh)) {
-		evt = cast_button_event(eh);
-
-		if (evt->pressed) {
-			LOG_INF("Pin Toggle");
-			ret = gpio_pin_toggle_dt(&led);
-		}
-	}
-
-	return true;
-}
-
-APP_EVENT_LISTENER(MODULE, event_handler);
-APP_EVENT_SUBSCRIBE(MODULE, button_event);

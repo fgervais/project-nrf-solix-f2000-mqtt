3e83a6e93888b94f1c3df9b763e2a9c744a17dc7
Francois Gervais <francoisgervais@gmail.com>
2023-09-04 09:56:22 -0400

    template: remove led0

--- app/src/main.c
+++ app/src/main.c
@@ -8,25 +8,9 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #include <app_version.h>
 
 
-#define SLEEP_TIME_MS   10
-#define LED0_NODE DT_ALIAS(myled0alias)
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-
 void main(void)
 {
-	int ret;
 	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-
-	if (!device_is_ready(led.port)) {
-		return;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return;
-	}
 
 	LOG_INF("Version: %s", APP_VERSION_FULL);
 
